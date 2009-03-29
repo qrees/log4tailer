ca74ae91751431b0b2e3de7915237db0ffaae894
jordilin <none@none>
2009-03-29 00:58:28 +0000

    / more testing for the Log class
    --HG--
    extra : convert_revision : svn%3A05628a4a-d5d7-11dd-a2b5-e5f827957e07/trunk%4059

--- tests/test_log.py
+++ tests/test_log.py
@@ -31,22 +31,61 @@ class TestLog(unittest.TestCase):
         '''creates a log file
         and opens it to make some tests'''
         self.logfile = open('out.log','w')
-        self.logfile.write("this is a line in the log\n")
+        self.aline = "this is a line in the log\n"
+        self.logfile.write(self.aline)
         self.logfile.close()
         self.logname = 'out.log'
+    
+    def openLog(self):
+        log = Log(self.logname)
+        log.openLog()
+        return log 
+
+    def writeNlines(self,nlines):
+        logfile = open(self.logname,'a')
+        for count in range(nlines):
+            logfile.write(self.aline)
+        logfile.close()
 
     def testLogCanRead(self):
-        log = Log(self.logname)
-        log.openLog()
-        self.assertEqual("this is a line in the log\n",log.readLine())
+        log = self.openLog()
+        self.assertEqual(self.aline,log.readLine())
 
     def testLogHasRotated(self):
-        log = Log(self.logname)
-        log.openLog()
+        log = self.openLog()
         self.tearDown()
         self.setUp()
         self.assertNotEqual(log.inode,log.getcurrInode)
     
+    def testLogSeekEnd(self):
+        log = self.openLog()
+        log.seekLogEnd()
+        # we should be at the end
+        self.assertEqual('',log.readLine())
+        log.closeLog()
+
+    def testLogNearlyEnd(self):
+        log = self.openLog()
+        log.seekLogNearlyEnd()
+        # log has been positioned 
+        # to read the last 10 lines
+        # by now. As we have only one line in the log
+        # make sure is the only one we read
+        # and is the same as the one in there
+        self.assertEqual(self.aline,log.readLine())
+        log.closeLog()
+        # test we read exactly last 10 lines
+        self.writeNlines(20)
+        log = self.openLog()
+        log.seekLogNearlyEnd()
+        # read till the end
+        count = 0
+        while log.readLine():
+            count += 1
+        self.assertEqual(10,count)
+
+
+
     def tearDown(self):
         os.remove(self.logname)
 
