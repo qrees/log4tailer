990e61153bd41a9e4d0a5def2ac0c7a16a2f473f
jordilin <none@none>
2009-03-29 20:00:39 +0000

    / a test to see if colors work well in terminal
    --HG--
    extra : convert_revision : svn%3A05628a4a-d5d7-11dd-a2b5-e5f827957e07/trunk%4067

---
+++ tests/test_colors.py
@@ -0,0 +1,44 @@
+import unittest
+import os,sys
+
+sys.path.append('..')
+from log4tailer.Log import Log
+from log4tailer.Message import Message
+from log4tailer.LogColors import LogColors
+from log4tailer.Actions.PrintAction import PrintAction
+
+class TestColors(unittest.TestCase):
+    def setUp(self):
+        self.logfile = 'out.log'
+        fh = open(self.logfile,'w')
+        self.someLogTraces = ['fatal> something went wrong',
+                              'error> not so wrong',
+                              'warn> be careful',
+                              'debug> looking behind the scenes',
+                              'info> the app is running']
+        for line in self.someLogTraces:
+            fh.write(line+'\n')
+        fh.close()
+
+    def testMessage(self):
+        logcolors = LogColors() #using default colors
+        target = None
+        action = PrintAction()
+        message = Message(logcolors,target)
+        log = Log(self.logfile)
+        log.openLog()
+        print "testing Colors..."
+        for count in range(len(self.someLogTraces)):
+            line = log.readLine()
+            line = line.rstrip()
+            message.parse(line)
+            action.triggerAction(message)
+
+    def tearDown(self):
+        os.remove(self.logfile)
+
+if __name__ == '__main__':
+        unittest.main()
+
+
+
