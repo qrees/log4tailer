a70ab62ddcf824a25d6ad94cff8d7211f63da391
jordilin <none@none>
2009-03-29 15:44:17 +0000

    / more tests for Properties module
    --HG--
    extra : convert_revision : svn%3A05628a4a-d5d7-11dd-a2b5-e5f827957e07/trunk%4066

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -3,8 +3,10 @@ import os,sys
 
 sys.path.append('..')
 from log4tailer.Properties import Property
+from log4tailer.log4Exceptions import KeyAlreadyExistsException,KeyNotFoundException
 
 class TestProperties(unittest.TestCase):
+    
     def setUp(self):
         self.configfile = 'config.txt'
         self.configfh = open(self.configfile,'w')
@@ -15,14 +17,33 @@ class TestProperties(unittest.TestCase):
         self.configfh.close()
         self.configKeys = colorconfigs.keys().sort()
 
-    def test_parseProperties(self):
+    def testparseProperties(self):
         property = Property(self.configfile)
         property.parseProperties()
         configPropertyKeys = property.getKeysLower().sort()
         # my colorconfigs keys are already in lowercase
         self.assertEqual(self.configKeys,configPropertyKeys)
-        
+    
+    def testKeyNotfoundException(self):
+        property = Property(self.configfile)
+        property.parseProperties()
+        key = 'hi'
+        self.assertRaises(KeyNotFoundException,property.getValue,key)
+    
+    def __createDuplicateKeysConfig(self):
+        os.remove(self.configfile)
+        configfh = open(self.configfile,'w')
+        colorconfigs = {'warn':'yellow','fatal':'red','error':'red'}
+        for key,value in colorconfigs.iteritems():
+            configfh.write(key +'='+value+'\n')
+        # making a duplicate level
+        configfh.write('warn'+'='+'red'+'\n')
+        configfh.close()
 
+    def testKeyAlreadyExistsException(self):
+        self.__createDuplicateKeysConfig()
+        property = Property(self.configfile)
+        self.assertRaises(KeyAlreadyExistsException,property.parseProperties)
 
     def tearDown(self):
         os.remove(self.configfile)
