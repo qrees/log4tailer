32ced378b7d197875023c45b4c06ae885612cf74
jordilin <none@none>
2010-05-16 10:42:53 +0000

    / warning keyword level colorized as well
    --HG--
    extra : convert_revision : svn%3A05628a4a-d5d7-11dd-a2b5-e5f827957e07/trunk%40340

--- log4tailer/Actions/PauseMode.py
+++ log4tailer/Actions/PauseMode.py
@@ -23,7 +23,7 @@ class PauseMode:
     not miss a level'''
 
     def __init__(self):
-        self.defaultLevelPauses = {'debug':0,'info':0, 'warn':0, 'error':0, 'fatal':0, 
+        self.defaultLevelPauses = {'debug':0,'info':0, 'warn':0, 'warning':0, 'error':0, 'fatal':0, 
                                    'critical':0,'target':0}
 
     def getPause(self,level):
