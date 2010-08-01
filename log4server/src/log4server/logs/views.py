cfc8d3b52a05d532fa2de0b2e47fdf54914d5941
jordilin <none@none>
2010-08-01 19:40:43 +0000

    decorator methods
    --HG--
    extra : convert_revision : svn%3A05628a4a-d5d7-11dd-a2b5-e5f827957e07/trunk%40532

--- log4server/src/log4server/logs/views.py
+++ log4server/src/log4server/logs/views.py
@@ -1,24 +1,50 @@
 # Create your views here.
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from models import Log
 
+try:
+    import json
+except:
+    import simplejson as json
+
+
+def allowed(verb):
+    def required(func):
+        def wrapper(request, *args, **kwargs):
+            if request.method != verb:
+                return HttpResponseNotFound()
+            return func(request, *args, **kwargs)
+        return wrapper
+    return required
+
+def from_json(model, request):
+    data_dict = json.loads(request.raw_post_data)
+    vals = {}
+    # model must have an static method 
+    # json mapper that returns a dict with callables 
+    # or None
+    to_model = model.json_mapper()
+    for key, value in data_dict.iteritems():
+        if key in to_model and callable(to_model[key]):
+            vals[key] = to_model[key](**value)
+        else:
+            vals[key] = value
+    return vals
+
+@allowed('POST')
 def alert(request):
-    if not request.method == 'POST':
-        raise Http404
     params = request.POST
     if not (params.get('logtrace', None) or 
             not params.get('logpath', None)):
-        raise Http404
+        return HttpResponseNotFound()
     log = Log(**params)
     log.save()
     return HttpResponse(content='', status=201)
 
+@allowed('GET')
 def info(request):
-    if not request.method == 'GET':
-        raise Http404
+    logs = Log.objects.all()
+    pass
     
-    #TODO must do a render to response after
-    #querying db
-    return HttpResponse(content='', status=200)
 
 
