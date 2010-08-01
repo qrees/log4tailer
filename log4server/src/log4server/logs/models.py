8e2e09bdbe3ab9b98fc014187299b947419ce157
jordilin <none@none>
2010-08-01 19:39:57 +0000

    logtrace model
    --HG--
    extra : convert_revision : svn%3A05628a4a-d5d7-11dd-a2b5-e5f827957e07/trunk%40531

--- log4server/src/log4server/logs/models.py
+++ log4server/src/log4server/logs/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Log(models.Model):
-    logpath = models.CharField(max_length=200)
-    logtrace = models.CharField(max_length=1000)
+    logpath = models.CharField(max_length=200, blank = False)
+    server = models.CharField(max_length=200, blank = False)
     
     def __unicode__(self):
         return self.logpath
-        
-    #def save(self, force_insert=False, force_update=False):
+
+class LogTrace(models.Model):
+    logtrace = models.ForeignKey(Log)
+    
+    def __unicode__(self):
+        return self.logtrace
         
