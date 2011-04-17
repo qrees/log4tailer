4757f8848e9264f83187c7c58fd8db428b3e01dc
jordilin <none@none>
2011-04-17 13:28:34 -0700

    check status in runtests

--- log4client/hudson_build.sh
+++ log4client/hudson_build.sh
@@ -3,7 +3,18 @@ CWD=`pwd`
 make env
 export PATH=$CWD/ENV/bin:$PATH
 make all
+
+# HACK to make the hudson job fail if tests fail
+sed -i -e 's/coverage.main()/sys.exit(coverage.main())/' bin/coverage
+
 make runtests
+
+# abort job if tests failed
+if [ $? -ne 0 ]; then
+    echo "Error while running the tests, please check"
+    exit -1
+fi
+
 make pylint > /dev/null 2>&1
 make release
 make coberturasource
