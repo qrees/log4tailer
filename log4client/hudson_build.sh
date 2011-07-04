377d0f678209333e4450702a924fb6e150563b5c
jordilin <none@none>
2011-07-04 12:22:00 -0700

    uses nosetests with branch coverage

--- log4client/hudson_build.sh
+++ log4client/hudson_build.sh
@@ -1,7 +1,7 @@
 cd $WORKSPACE/log4client
 CWD=`pwd`
 COVERAGE=bin/coverage
-TEST=bin/py.test
+TEST=bin/test 
 
 make env
 export PATH=$CWD/ENV/bin:$PATH
@@ -9,14 +9,14 @@ make all
 
 # HACK to make the hudson job fail if tests fail
 sed -i -e 's/coverage.main()/sys.exit(coverage.main())/' bin/coverage
-sed -i -e 's/pytest.main()/sys.exit(pytest.main())/' bin/py.test
+#sed -i -e 's/pytest.main()/sys.exit(pytest.main())/' bin/py.test
 
-$COVERAGE run $TEST -v --junitxml=unittests.xml tests
+$COVERAGE run --pylib --branch --source=src/log4tailer $TEST -v --with-xunit --xunit-file=unittests.xml
 if [ $? -ne 0 ]; then
     echo "Error while running the tests, please check"
     exit -1
 fi
-$COVERAGE xml src/log4tailer/*.py
+$COVERAGE xml 
 make pylint > /dev/null 2>&1
 make release
 make coberturasource
