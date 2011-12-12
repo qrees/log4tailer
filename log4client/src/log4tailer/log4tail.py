2d13d5d32045153a4894aba192232f599ce98e07
jordilin <none@none>
2011-12-11 16:25:37 -0800

    refactored cli

--- log4client/src/log4tailer/log4tail.py
+++ log4client/src/log4tailer/log4tail.py
@@ -22,18 +22,19 @@ import sys
 from optparse import OptionParser
 import log4tailer
 
+
 def startupNotice():
     notice = """Log4Tailer  Copyright (C) 2008 Jordi Bosch
-        This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
-            This is free software, and you are welcome to redistribute it
-                under certain conditions; type `show c' for details."""
+     This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+           This is free software, and you are welcome to redistribute it
+               under certain conditions; type `show c' for details."""
     print notice
 
-def parse_command_line():
-    if len(sys.argv[1:]) == 0:
+
+def log4tail_cli(parser, args):
+    if len(args) == 0:
         print "Provide at least one log"
         sys.exit()
-    parser = OptionParser()
     parser.add_option("-c", "--config", dest="configfile",
             help="config file with colors")
     parser.add_option("-p", "--pause", dest="pause",
@@ -48,9 +49,9 @@ def parse_command_line():
             help="prints last N lines from log")
     parser.add_option("-t", "--target", dest="target",
             help="emphasizes a line in the log")
-    parser.add_option("-m","--mail", action="store_true", dest="mail",
+    parser.add_option("-m", "--mail", action="store_true", dest="mail",
             help="notification by mail when a fatal is found")
-    parser.add_option("-r","--remote", action="store_true", dest="remote",
+    parser.add_option("-r", "--remote", action="store_true", dest="remote",
             help="remote tailing over ssh")
     parser.add_option("-f", "--filter", dest="filter",
             help="filters log traces, tail and grep")
@@ -59,7 +60,8 @@ def parse_command_line():
     parser.add_option("--cornermark", dest="cornermark",
             help="displays a mark in bottom right corner of terminal")
     parser.add_option("--no-mail-silence", action="store_true",
-            dest="nomailsilence", help="silent mode but no specific notification")
+            dest="nomailsilence",
+            help="silent mode but no specific notification")
     parser.add_option("--executable", action="store_true",
             dest="executable", help="executes a program")
     parser.add_option("--version", action="store_true",
@@ -69,13 +71,12 @@ def parse_command_line():
     parser.add_option("--screenshot", action="store_true", dest="screenshot",
         help="takes a terminal screenshot whenever it finds an alertable log "
                 "trace")
-
     return parser.parse_args()
 
+
 def main():
-    options, args = parse_command_line()
+    options, args = log4tail_cli(OptionParser(), sys.argv)
     log4tailer.main(options, args)
 
 if __name__ == '__main__':
     main()
-
---
+++ log4client/tests/test_cli.py
@@ -0,0 +1,47 @@
+# Log4Tailer: A multicolored python tailer for log4J logs
+# Copyright (C) 2011 Jordi Carrillo Bosch
+
+# This file is part of Log4Tailer Project.
+#
+# Log4Tailer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Log4Tailer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Log4Tailer.  If not, see <http://www.gnu.org/licenses/>.
+#####################################################
+
+import unittest
+from log4tailer.log4tail import log4tail_cli
+from optparse import OptionParser
+from tests.utils import MemoryWriter
+import sys
+from testtools.monkey import patch
+
+
+class TestCli(unittest.TestCase):
+    def setUp(self):
+        self.mwr = MemoryWriter()
+        patch(sys, 'stdout', self.mwr)
+
+    def test_cliparser_nologprovided(self):
+        cli_args = []
+        expected_exit = "Provide at least one log"
+        try:
+            log4tail_cli(OptionParser(), cli_args)
+            self.fail()
+        except SystemExit:
+            self.assertTrue(expected_exit in self.mwr.captured)
+
+    def test_cliparser_hascornermarkoption(self):
+        cli_args = ["justonelog"]
+        options, _ = log4tail_cli(OptionParser(), cli_args)
+        self.assertTrue(hasattr(options, "configfile"))
+        self.assertTrue(hasattr(options, "cornermark"))
+        self.assertTrue(hasattr(options, "silence"))
