4ba2db36ffa201c57fcef893128218cb96330ca5
jordilin <none@none>
2011-12-30 21:08:52 -0800

    refactoring tests cornermark

--- log4client/tests/test_cornermark.py
+++ log4client/tests/test_cornermark.py
@@ -2,23 +2,29 @@
 
 import unittest
 import sys
-import os
 from log4tailer.logfile import Log
 from log4tailer.message import Message
 from log4tailer.logcolors import LogColors
 from log4tailer import notifications
 from log4tailer.termcolorcodes import TermColorCodes
-from log4tailer.propertyparser import Property
 from .utils import MemoryWriter
 
 
-CONFIG = 'aconfig.txt'
+class PropertiesStub(object):
+    def __init__(self):
+        pass
+
+    def get_value(self, value):
+        return "targeted"
+
 
 def overgap():
     return 0.02
 
+
 def tcols_callback():
     return 5
+
 
 def patch_term_num_cols(notifier):
     notifier.term_num_cols = tcols_callback
@@ -27,11 +33,11 @@ def patch_term_num_cols(notifier):
 class TestCornerMark(unittest.TestCase):
     """The purpose of the corner mark is to have an small square in the bottom
     right corner of the terminal to indicate something went wrong.
-    """ 
+    """
     def setUp(self):
         self.sysback = sys.stdout
         self.ttcols = tcols_callback()
-    
+
     def testIsCornerMark(self):
         cornermark = notifications.CornerMark(10)
         self.assertEquals(10, cornermark.corner_mark_time())
@@ -95,8 +101,10 @@ class TestCornerMark(unittest.TestCase):
         patch_term_num_cols(notifier)
         notifier.notify(message, anylog)
         self.assertFalse(sys.stdout.captured)
+
         def belowgap():
             return 0
+
         notifier.timer.corner_mark_ellapsed = belowgap
         level = 'FATAL'
         trace = "FATAL there could be an error in the application"
@@ -131,20 +139,14 @@ class TestCornerMark(unittest.TestCase):
         self.assertEquals(output, sys.stdout.captured[2])
 
     def testMarkedTARGET(self):
-        configfile = CONFIG
         logfile = "/any/path/out.log"
         trace = "this is a targeted log trace"
-        fh = open(configfile, 'w')
-        fh.write("targets "+logfile+"=targeted\n")
-        fh.close()
-        properties = Property(configfile)
-        properties.parse_properties()
         sys.stdout = MemoryWriter()
         logcolors = LogColors()
         termcolors = TermColorCodes()
         notifier = notifications.CornerMark(0.02)
-        anylog = Log(logfile, properties)
-        message = Message(logcolors, properties = properties)
+        anylog = Log(logfile, PropertiesStub())
+        message = Message(logcolors, properties=PropertiesStub())
         padding = self.ttcols - len(notifier.MARK)
         output = padding * " " + termcolors.oncyanemph + notifier.MARK +\
                 termcolors.reset
@@ -154,20 +156,14 @@ class TestCornerMark(unittest.TestCase):
         self.assertEqual(output, sys.stdout.captured[0])
 
     def testMarkedTARGETOverMarkableLevel(self):
-        configfile = CONFIG
         logfile = "/any/path/out.log"
         trace = "this is a FATAL targeted log trace"
-        fh = open(configfile, 'w')
-        fh.write("targets "+logfile+"=targeted\n")
-        fh.close()
-        properties = Property(configfile)
-        properties.parse_properties()
         sys.stdout = MemoryWriter()
         logcolors = LogColors()
         termcolors = TermColorCodes()
         notifier = notifications.CornerMark(0.02)
-        anylog = Log(logfile, properties)
-        message = Message(logcolors, properties = properties)
+        anylog = Log(logfile, PropertiesStub())
+        message = Message(logcolors, properties=PropertiesStub)
         padding = self.ttcols - len(notifier.MARK)
         output = padding * " " + termcolors.oncyanemph + notifier.MARK +\
                 termcolors.reset
@@ -175,12 +171,6 @@ class TestCornerMark(unittest.TestCase):
         patch_term_num_cols(notifier)
         notifier.notify(message, anylog)
         self.assertEqual(output, sys.stdout.captured[0])
-    
+
     def tearDown(self):
-        if os.path.exists(CONFIG):
-            os.remove(CONFIG)
         sys.stdout = self.sysback
-
-if __name__ == '__main__':
-    unittest.main()
-
